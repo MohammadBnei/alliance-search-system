40cf81aad0ec822f6eed4956ba5e5b9d75a6960f
mohammadbnei <mohammadamine.banaei@gmail.com>
2020-12-07 10:18:20 +0100

    Modified typo

--- api-swapi/src/config/server.js
+++ api-swapi/src/config/server.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
+
 module.exports = {
     PORT: process.env.PORT || 3000,
     ssl: {
-        key: fs.readFileSync(proces.env.SSL_KEY),
-        cert: fs.readFileSync(proces.env.SSL_CERT)
+        key: fs.readFileSync(process.env.SSL_KEY),
+        cert: fs.readFileSync(process.env.SSL_CERT)
     },
 }
